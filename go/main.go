65741d2ba3986ca5074a810e37ea7deee23aa70b
urothis <natecaple@gmail.com>
2019-08-23 17:38:29 -0500

    untested

--- go/main.go
+++ go/main.go
@@ -8,10 +8,15 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/janeczku/go-spinner"
 	minio "github.com/minio/minio-go/v6"
 	"github.com/schollz/progressbar/v2"
 )
+
+// Client Connection
+type Client struct {
+	client *minio.Client
+	Bucket string
+}
 
 func fmtDuration(d time.Duration) string {
 	d = d.Round(time.Minute)
@@ -21,7 +26,7 @@ func fmtDuration(d time.Duration) string {
 	return fmt.Sprintf("%02dh%02dm", h, m)
 }
 
-func fileCount(path string) (int, error) {
+func localFileCount(path string) (int, error) {
 	i := 0
 	err := filepath.Walk(path,
 		func(path string, info os.FileInfo, err error) error {
@@ -39,54 +44,40 @@ func fileCount(path string) (int, error) {
 	return i, nil
 }
 
-func main() {
-	start := time.Now()
-
-	endpoint := os.Getenv("ENDPOINT")
-	accessKeyID := os.Getenv("ACCESS_KEY")
-	secretAccessKey := os.Getenv("SECRET_KEY")
-
-	// Initiate a client using DigitalOcean Spaces.
-	client, err := minio.New(endpoint, accessKeyID, secretAccessKey, true)
-	if err != nil {
-		log.Fatal(err)
-	}
-	spaceName := os.Getenv("SPACE_NAME")
-
-	objectsCh := make(chan string)
+func purgeOldNWSyncData(client *minio.Client, spaceName, objectPrefix string) int {
 	log.Println("Purging old nwsync data from cdn")
-	s := spinner.StartNew("This may take some time...")
-	// Send object names that are needed to be removed to objectsCh
-	go func() {
-		defer close(objectsCh)
 
-		doneCh := make(chan struct{})
+	doneCh := make(chan struct{})
+	defer close(doneCh)
 
-		// Indicate to our routine to exit cleanly upon return.
-		defer close(doneCh)
-
-		// List all objects from a bucket-name with a matching prefix.
-		for object := range client.ListObjects(spaceName, "nwsync/"+os.Getenv("MODULENAME")+"/", true, doneCh) {
-			if object.Err != nil {
-				log.Fatalln(object.Err)
-			}
-			objectsCh <- object.Key
+	// List all objects from a bucket-name with a matching prefix.
+	objectsCh := make(chan string)
+	count := 0
+	for object := range client.ListObjects(spaceName, objectPrefix, true, doneCh) {
+		if object.Err != nil {
+			log.Fatalln(object.Err)
 		}
-	}()
+		count++
+		objectsCh <- object.Key
+	}
 
 	// Call RemoveObjects API
 	errorCh := client.RemoveObjects(spaceName, objectsCh)
-
 	// Print errors received from RemoveObjects API
 	for e := range errorCh {
 		log.Fatalln("Failed to remove " + e.ObjectName + ", error: " + e.Err.Error())
 	}
-	s.Stop()
-	log.Println("\n")
-	log.Println("Old nwsync data purged")
 
-	folder := "/nwsync"
-	totalCount, _ := fileCount(folder)
+	log.Println("\n Old nwsync data purged")
+
+	return count
+}
+
+func uploadNewNWSyncData(client *minio.Client, folder, spaceName string) (int, error) {
+	totalCount, err := localFileCount("/nwsync")
+	if err != nil {
+		return 0, err
+	}
 	log.Printf("%d files processing", int(totalCount))
 	bar := progressbar.NewOptions(totalCount,
 		progressbar.OptionSetRenderBlankState(true),
@@ -98,6 +89,7 @@ func main() {
 			if info, err := os.Stat(path); err == nil && info.IsDir() {
 				return err
 			}
+
 			bar.Add(1)
 			// upload object
 			_, err = client.FPutObject(spaceName, "nwsync/"+os.Getenv("MODULENAME")+path[7:], path, minio.PutObjectOptions{ContentType: "application/gzip", UserMetadata: map[string]string{"x-amz-acl": "public-read"}})
@@ -109,7 +101,38 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-	Totalelapsed := time.Since(start)
 	bar.Finish()
-	log.Printf("Successfully uploaded %d files | %s elapsed\n", int(totalCount), fmtDuration(Totalelapsed))
+	return totalCount, err
+}
+
+func main() {
+	start := time.Now()
+
+	endpoint := os.Getenv("ENDPOINT")
+	accessKeyID := os.Getenv("ACCESS_KEY")
+	secretAccessKey := os.Getenv("SECRET_KEY")
+	spaceName := os.Getenv("SPACE_NAME")
+	localPath := os.Getenv("NWSYNCFILEPATH")
+	objectPrefix := "nwsync/" + os.Getenv("MODULENAME") + "/"
+
+	// Initiate a client
+	client, err := minio.New(endpoint, accessKeyID, secretAccessKey, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// clean the old nwsync data
+	deletedCount := purgeOldNWSyncData(client, spaceName, objectPrefix)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// upload new files
+	totalCount, err := uploadNewNWSyncData(client, localPath, spaceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Totalelapsed := time.Since(start)
+	log.Printf("Successfully deleted %d old files | uploaded %d new files | %s elapsed\n", int(deletedCount), int(totalCount), fmtDuration(Totalelapsed))
 }
